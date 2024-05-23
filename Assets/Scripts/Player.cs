9da2be5615d903a092163fc66d9c6e0028aed392
Berkay Kiziltug <berkay.kzt@gmail.com>
2024-05-23 18:59:06 +0300

    Seperate game inpÄut

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,30 +5,12 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float rotateSpeed = 10f;
+    [SerializeField] private GameInput _gameInput;
     private bool isWalking;
    
     private void Update()
     {
-        Vector2 input = new Vector2(0, 0);
-        if(Input.GetKey(KeyCode.W))
-        {
-            input.y = +1;
-        }
-        if(Input.GetKey(KeyCode.S))
-        {
-            input.y = -1;
-        }
-        
-        if(Input.GetKey(KeyCode.A))
-        {
-            input.x = -1;
-        }
-        if(Input.GetKey(KeyCode.D))
-        {
-            input.x = +1;
-        }
-        
-        input = input.normalized;
+        Vector2 input = _gameInput.GetMovementVectorNormalized();     
         Vector3 moveDir = new Vector3(input.x, 0, input.y);
         isWalking = moveDir != Vector3.zero;
         transform.position += moveDir * moveSpeed * Time.deltaTime;
